99dac2ba8a058b3f575160f7482131af5717b77d
joeeames <joeeames@gmail.com>
2013-05-16 00:52:59 -0600

    removing idea files, restored cart tests

--- test/unit/cartSpec.js
+++ test/unit/cartSpec.js
@@ -1,6 +1,26 @@
 'use strict';
 
 describe('cart', function() {
+  var d3 = {id: 3, name: 'diablo III', cost: 24.99};
   beforeEach(module('myApp.services'));
 
+
+  beforeEach(module(function($provide) {
+    $provide.value('catalog', {items: [d3]})
+  }));
+
+  describe('add', function() {
+    it('should add diablo 3 with qty of 1 to the items', inject(function(cart) {
+      cart.addItem(3, 1);
+      expect(cart.items.length).toEqual(1);
+      expect(cart.items[0]).toEqual({id: 3, name: 'diablo III', cost: 24.99, qty: 1});
+    }));
+
+    it('should add diablo 3 with qty of 2 when 2 are added', inject(function(cart) {
+      cart.addItem(3, 2);
+      expect(cart.items.length).toEqual(1);
+      expect(cart.items[0]).toEqual({id: 3, name: 'diablo III', cost: 24.99, qty: 2});
+
+    }));
+  });
 });
