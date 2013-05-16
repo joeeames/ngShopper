0b28b94b3c114f7b4ffde533b83d1fc8cab90318
joeeames <joeeames@gmail.com>
2013-05-16 00:56:44 -0600

    first test

--- app/js/cart.js
+++ app/js/cart.js
@@ -3,7 +3,12 @@
 angular.module('myApp.services').service('cart', function(catalog) {
   return {
     items: [],
-    
+    addItem: function(id, qty) {
+      var foundItem = _.find(catalog.items, function(catItem) {
+        return catItem.id === id;
+      });
+      this.items.push({id: id, name: foundItem.name, cost: foundItem.cost, qty: 1});
+    }
   }
 
 
