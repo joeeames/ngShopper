ee29f467877de7fc6f1b74c97f58a7bf66168d9a
joeeames <joeeames@gmail.com>
2013-08-26 23:33:19 -0600

    updated karma to latest version

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,24 +1,28 @@
-basePath = '../';
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
+    frameworks: ['jasmine'],
+    files: [
+      'app/lib/underscore.js',
+      'app/lib/angular/angular.js',
+      'app/lib/angular/angular-*.js',
+      'test/lib/angular/angular-mocks.js',
+      'test/lib/sinon-1.7.1.js',
+      'app/js/controllers.js',
+      'app/js/services.js',
+      'app/js/**/*.js',
+      'test/unit/**/*.js'
+    ],
+    preprocessors: {
 
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/lib/underscore.js',
-  'app/lib/angular/angular.js',
-  'app/lib/angular/angular-*.js',
-  'test/lib/angular/angular-mocks.js',
-  'test/lib/sinon-1.7.1.js',
-  'app/js/controllers.js',
-  'app/js/services.js',
-  'app/js/**/*.js',
-  'test/unit/**/*.js'
-];
-
-autoWatch = true;
-
-browsers = ['Chrome'];
-
-junitReporter = {
-  outputFile: 'test_out/unit.xml',
-  suite: 'unit'
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    captureTimeout: 60000,
+    singleRun: false
+  });
 };
